ca39486b0c169e9bafbc7fcd4df6229b364380a9
Ben Bozzay <ben@fullstackdigital.com>
2018-10-18 11:56:21 -0400

    updated gui

--- static/js/pancakes_debug.js
+++ static/js/pancakes_debug.js
@@ -1,14 +1,23 @@
-//add draggable
+//ADD draggable
 //https://github.com/Shopify/draggable
 
 const body = document.querySelector('body');
-
+const sectionHTML = document.querySelector('.debugging-bar').innerHTML;
 //body.innerHTML = body.innerHTML + 
 //``;
 
 const sections = document.querySelectorAll('section');
 const rows = document.querySelectorAll('#row');
 const columns = document.querySelectorAll('div[data-column-size]');
+const elements = document.querySelectorAll('.elements-wrapper');
+
+body.classList.add("inspect-mode");
+document.querySelector('div[data-column-size]').setAttribute('data-highlightable','1');
+
+// DEBUG BAR ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const previewBtn = document.querySelector('.enable-debug');
+const exportYML = document.querySelector('.export-yml');
 
 // Pages menu
 const pagesDrawer = document.querySelector(".dbg-pages-container");
@@ -16,13 +25,41 @@ const pagesBtn = document.querySelector(".dbg-btn");
 // Call the function
 //section.addEventListener("click", functionName);
 
+// Toggle pages drawer on click
+pagesBtn.addEventListener("click", () => {
+  pagesDrawer.classList.toggle("debug-menu-active");
+});
 
+// Toggle inspect mode/preview mode on click
+previewBtn.addEventListener("click", () => {
+  body.classList.toggle("inspect-mode");
+});
+
+
+
+// INSPECT MODE
+/////////////////////////////////////////////////////////////
+if ( body.classList.contains("inspect-mode") ) {
 //do stuff for each section
 sections.forEach(function (section, index) {
-  console.log(section);
-  section.style.border = "1px solid #000";
+  //console.log(section);
+  section.setAttribute('data-highlightable','1');
 
-  createDebugMenu(section);
+  //localStorage.setItem("section", JSON.stringify(section) );
+
+  section.addEventListener("mouseenter", () => {
+    createDebugMenu(section);
+  });
+
+  section.addEventListener("mouseleave", () => {
+    destroyDebugMenu(section);
+  });
+
+  section.addEventListener("click", () => {
+    sectionClick(section);
+  });
+
+  
   // Change the headers to include the classlist 
   let section_h1 = section.querySelector('h1');
   let sectionTitle = section.getAttribute('data-section-name');
@@ -32,7 +69,16 @@ sections.forEach(function (section, index) {
   //section_h1.innerText = section.classList;
 
   //Toggle blue outlines for columns on this.hover
+  // This takes a snapshot of the HTML before the JS is loaded
+  localStorage.setItem('html', body.innerHTML);
+  // Export YML on click
+  exportYML.addEventListener("click", () => {
+    body.innerHTML = localStorage.getItem('html');
+    AOS.init();
+  });
   rows.forEach(function (row, index) {
+
+    row.setAttribute('data-highlightable','1');
 
     row.addEventListener("mouseover", () => {
       event.target.style.outline = "1px solid blue";
@@ -45,6 +91,16 @@ sections.forEach(function (section, index) {
 
     //Toggle green outlines for columns on this.hover
     columns.forEach(function (column, index) {
+
+      column.setAttribute('data-highlightable','1');
+
+      const columnAttributes = {
+        size: column.getAttribute('data-column-size')
+      }
+
+      // window.localStorage.setItem('columnAttributes', JSON.stringify(columnAttributes))
+
+      // console.log("COL SIZE:" + columnAttributes.size);
 
       column.addEventListener("mouseenter", () => {
         event.target.style.outline = "1px solid green";
@@ -66,53 +122,64 @@ sections.forEach(function (section, index) {
           column.style = "";
       });
 
+      elements.forEach(function (element, index) {
+        element.setAttribute('data-highlightable','1');
+      });
+
     });
   }); 
 });
 
 
-// DEBUG BAR ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function sectionClick(sectionName, index) {
+  if ( document.querySelector('section.active') !== null ) {
+    document.querySelector('section.active').classList.remove('active');
+  }
+  sectionName.classList.toggle("active");
+}
 
-// Toggle pages drawer on click
-pagesBtn.addEventListener("click", () => {
-  pagesDrawer.classList.toggle("debug-menu-active");
-});
+let debugWidget = `<div id="section-dbg-menu" class="dbg-each-menu">
+  
+<div class="dbg-main-btns">
+
+<i class="fas fa-arrows-alt"></i>
+<span class="this-section-name"></span>
+<a class="prepend-me part-edit" title="Open the Page Editor for this Part" href="https://app.forestry.io/sites/site_id/#/pages/content-{{ $.Scratch.Get "part_preview_link" }}"><i class="fas fa-external-link-alt"></i></a>
+
+<i class="fas fa-pen-square"></i>
+
+</div>
+<div class="dbg-style-menu">
+<ul class="dbg-style-sub-parent dbg-style-padding">
+<span class="dbg-style-sub-parent">Padding <i class="fas fa-caret-down"></i></span>
+<ul class="dbg-style-padding-top">
+    <li class="dbg-list-sub-title dbg-style-padding-top-title">Top: </li>   
+    <ul class="dbg-style-padding-bottom">
+        <li class="dbg-list-sub-title dbg-style-padding-bottom-title">Bottom: </li> 
+    </ul>
+</ul>
+</ul>
+<ul class="dbg-style-sub-parent dbg-style-background">
+<span class="dbg-style-sub-parent">Background...</i></span>
+</ul>
+<ul class="dbg-style-sub-parent dbg-style-section-height">
+<span class="dbg-style-sub-parent">Height <i class="fas fa-caret-down"></i></span>
+<ul class="dbg-style-section-height-options">
+    
+</ul>
+</ul>
+</div><!--dbg-style-menu-->
+</div><!--dbg-each-menu-->`;
 
 // Create hover menus
-
 function createDebugMenu(sectionName, index) {
   //console.log(rowName);
-  sectionName.innerHTML = sectionName.innerHTML + 
-  `<div id="section-dbg-menu" class="dbg-each-menu">
-  
-      <div class="dbg-main-btns">
-  
-      <i class="fas fa-arrows-alt"></i>
-      <span class="this-section-name"></span>
-      <a class="prepend-me part-edit" title="Open the Page Editor for this Part" href="https://app.forestry.io/sites/site_id/#/pages/content-{{ $.Scratch.Get "part_preview_link" }}"><i class="fas fa-external-link-alt"></i></a>
-  
-      <i class="fas fa-pen-square"></i>
-  
-      </div>
-  <div class="dbg-style-menu">
-  <ul class="dbg-style-sub-parent dbg-style-padding">
-  <span class="dbg-style-sub-parent">Padding <i class="fas fa-caret-down"></i></span>
-      <ul class="dbg-style-padding-top">
-          <li class="dbg-list-sub-title dbg-style-padding-top-title">Top: </li>   
-          <ul class="dbg-style-padding-bottom">
-              <li class="dbg-list-sub-title dbg-style-padding-bottom-title">Bottom: </li> 
-          </ul>
-      </ul>
-  </ul>
-  <ul class="dbg-style-sub-parent dbg-style-background">
-      <span class="dbg-style-sub-parent">Background...</i></span>
-  </ul>
-  <ul class="dbg-style-sub-parent dbg-style-section-height">
-      <span class="dbg-style-sub-parent">Height <i class="fas fa-caret-down"></i></span>
-      <ul class="dbg-style-section-height-options">
-          
-      </ul>
-  </ul>
-  </div><!--dbg-style-menu-->
-  </div><!--dbg-each-menu-->`;
+  debugWidget = debugWidget; 
+  sectionName.innerHTML = sectionName.innerHTML + debugWidget;
 }
+function destroyDebugMenu(sectionName, index) {
+  //console.log(rowName);
+  sectionName.innerHTML = sectionName.innerHTML;
+}
+
+} // End if body class contains inspect mode
